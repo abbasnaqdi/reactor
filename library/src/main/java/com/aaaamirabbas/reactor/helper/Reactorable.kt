9df3ecece04bd20bc1051e4581fa70b9f8c5f992
AMIR ABBAS NAGHDI <naghdi.official@gmail.com>
2021-11-27 09:49:11 +0330

    update gradle settings

--- library/src/main/java/com/aaaamirabbas/reactor/helper/Reactorable.kt
+++ library/src/main/java/com/aaaamirabbas/reactor/helper/Reactorable.kt
@@ -1,5 +1,9 @@
 package com.aaaamirabbas.reactor.helper
 
+import android.os.Parcelable
 import java.io.Serializable
 
 interface ReactorContract : Serializable
+
+// TODO: do it
+private interface ReactorParcellabel : Parcelable
